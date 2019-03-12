833899bba2bfddefd7f52f5210368fd3d2a85bed
Gustav <g.often94@gmail.com>
2019-03-12 13:25:44 +0100

    added motor pu-model

---
+++ simulink_gocart/pu_model_motor.m
@@ -0,0 +1,17 @@
+Ua_base = 93.92;
+Ia_base = 160;
+Za_base = Ua_base/Ia_base;
+tau_base = 68.8;
+N_base = 1900;
+J_m = 0.06;
+J_gear = 0.1;
+omega_base = N_base*pi/30;
+Uf_base = 75;
+If_base = 7.5;
+Zf_base = 10;
+ny = 0.904;
+r_a = 0.0646;
+l_a = 3.56e-3;
+r_f = 0.79;
+T_a = l_a/r_a;
+T_m = (J_m+J_gear)*omega_base^2/Ua_base/Ia_base;
